e233577fd50d2e20e639cf656c7c93064ee5a685
bud-caolibin <caolibin@budcreate.io>
2023-03-26 00:29:51 +0800

    feat:初始化behavior

---
+++ behavior/tank_behavior.py
@@ -0,0 +1,9 @@
+from interface.behavior import IBehavior
+
+
+class TankBehavior(IBehavior):
+
+    def __init__(self):
+        super(TankBehavior, self).__init__()
+
+
---
+++ interface/behavior.py
@@ -0,0 +1,11 @@
+
+
+class IBehavior(object):
+
+    def __init__(self):
+        self.model = None
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+
