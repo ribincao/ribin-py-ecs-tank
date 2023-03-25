505f3d1df8b316d67b9d4116efe41e2c72c26775
RibinCao <48990691+ribincao@users.noreply.github.com>
2023-03-25 14:49:24 +0800

    fix:更新 tank_game.py

--- game/tank_game.py
+++ game/tank_game.py
@@ -1,12 +1,11 @@
 from interface.game import IGame
-from manager.context import Context
-from typing import Dict
+from interface.context import IContext
+
 
 class TankGame(IGame):
 
-    def __init__(self, gid: int):
-        super(TankGame, self).__init__(gid)
-        self.context: Context = Context()
+    def __init__(self, gid: int, context: IContext):
+        super(TankGame, self).__init__(gid, context)
     
     def init_game(self):
         from system.move_system import MoveSystem
@@ -14,9 +13,9 @@ class TankGame(IGame):
         from system.command_system import CommandSystem
         from system.collider_system import ColliderSystem
         
-        self.register(MoveSystem(self.context))
-        self.register(CreateSystem(self.context))
-        self.register(CommandSystem(self.context))
-        self.register(ColliderSystem(self.context))
+        self.register_system(MoveSystem(self.context))
+        self.register_system(CreateSystem(self.context))
+        self.register_system(CommandSystem(self.context))
+        self.register_system(ColliderSystem(self.context))
     
 
