fc8da1dec2eedda1289ac747ecf627f856920a43
RibinCao <48990691+ribincao@users.noreply.github.com>
2023-03-25 14:45:18 +0800

    更新 game 接口

--- interface/game.py
+++ interface/game.py
@@ -1,11 +1,19 @@
-from abc import abstractmethod
+from interface.system import ISystem
+from typing import Dict
 
 
 class IGame(object):
 
     def __init__(self, gid: int):
         self.gid: int = gid
+        self.systems: Dict[str, ISystem] = {}
+    
+    def register_system(self, system: ISystem):
+        system_name = system.__class__.__name.__
+        if system_name in self.systems:
+            print("warning")
+        self.systems[system_name] = system
 
-    @abstractmethod
     def update(self):
-        pass
+        for _, system in self.systems.items():
+            system.update()
