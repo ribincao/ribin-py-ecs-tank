d4961489a01bf8d050f556d806d0773337615b5a
RibinCao <48990691+ribincao@users.noreply.github.com>
2023-03-25 15:07:57 +0800

    fix:更新 main.py

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from common.common import signal_handler
 import asyncio
 from typing import Optional
 from interface.game import IGame
-from manager.context import Context
-from manager.system_manager import SystemManager
+from context import Context
+from game.tank_game import TankGame
 
 
-class Client(object):
+class Tank(object):
 
     def __init__(self):
-        self.game: Optional[IGame] = None
+        self.context: Context = Context()
+        self.logic: IGame = TankGame(1, self.context)
+        # self.connection = None # 用于网络连接
+        # self.window = None #用来监听和渲染ui
         self._loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
-
-
+        
+    def init_game(self):
+        self.logic.init_system()
+        
     def run(self):
-        self._loop.create_task(self.game.update())
+        self._loop.create_task(self.logic.update())
         self._loop.run_forever()
 
 
