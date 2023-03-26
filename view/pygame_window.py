95f0660901457d2c90066a7295ad20d1de4ffc2c
ribincao <njucaolibin@163.com>
2023-03-26 14:39:19 +0800

    fix:一些测试代码

--- view/pygame_window.py
+++ view/pygame_window.py
@@ -2,6 +2,7 @@ from interface.window import IWindow
 from common.logger import logger
 import asyncio
 import pygame
+from typing import Optional
 from interface.context import IContext
 
 
@@ -10,11 +11,19 @@ class PyGameWindow(IWindow):
 
     def __init__(self, window_name: str, context: IContext):
         super(PyGameWindow, self).__init__(window_name, context)
-        self.window = None
+        self.window: Optional[pygame.Surface] = None
+
+        self.t_img = pygame.image.load("./resource/tank/imge/player/p1tankD.gif")
+        self.t_rect = self.t_img.get_rect()
+        self.t_rect.left = 0
+        self.t_rect.top = 0
 
     async def update(self):
         while True:
+            self.window.fill(pygame.Color(0, 0, 0))
             self.listen_event()
+
+            self.window.blit(self.t_img, self.t_rect)
             pygame.display.update()
             await asyncio.sleep(self.RENDER_RATE)
 
@@ -25,8 +34,8 @@ class PyGameWindow(IWindow):
 
     def listen_event(self):
         # StartScene
-        if not self.in_game:
-            return self.start_scene()
+        # if not self.in_game:
+        #     return self.start_scene()
         return self.game_scene()
 
     def game_scene(self):
@@ -40,12 +49,34 @@ class PyGameWindow(IWindow):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
                     logger.debug("w keydown")
+                    self.t_rect.top -= 1
                 elif event.key == pygame.K_a:
                     logger.debug("a keydown")
+                    self.t_rect.left -= 1
                 elif event.key == pygame.K_s:
                     logger.debug("s keydown")
+                    self.t_rect.top += 1
                 elif event.key == pygame.K_d:
                     logger.debug("d keydown")
+                    self.t_rect.left += 1
 
     def start_scene(self):
-        pass
+        event_list = pygame.event.get()
+        for event in event_list:
+            if event.type == pygame.QUIT:
+                import sys
+                sys.exit(0)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    logger.debug("w keydown")
+                    self.t_rect.top -= 1
+                elif event.key == pygame.K_a:
+                    logger.debug("a keydown")
+                    self.t_rect.left -= 1
+                elif event.key == pygame.K_s:
+                    logger.debug("s keydown")
+                    self.t_rect.top += 1
+                elif event.key == pygame.K_d:
+                    logger.debug("d keydown")
+                    self.t_rect.left += 1
