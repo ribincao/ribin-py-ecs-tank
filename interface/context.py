7a2d6812797c45ab327cf5bed797d091aa6a16f7
ribincao <njucaolibin@163.com>
2023-03-26 16:40:03 +0800

    fix:更新一下context类的接口

--- interface/context.py
+++ interface/context.py
@@ -1,6 +1,8 @@
 from interface.behavior import IBehavior
 from interface.entity import IEntity
+from interface.command import ICommand
 from typing import List
+from abc import abstractmethod
 
 
 class IContext(object):
@@ -8,8 +10,26 @@ class IContext(object):
     def __init__(self):
         pass
 
+    @abstractmethod
     def get_behaviors(self) -> List[IBehavior]:
         return []
 
+    @abstractmethod
     def get_entities(self) -> List[IEntity]:
         return []
+    
+    @abstractmethod
+    def create_entity(self, eid: int) -> IEntity:
+        return IEntity(-1)
+
+    @abstractmethod
+    def filter_entity(self, component: str, entities: List[IEntity]):
+        pass
+
+    @abstractmethod
+    def remove_entity(self, eid: int):
+        pass
+
+    @abstractmethod
+    def input_command(self, command: ICommand):
+        pass
