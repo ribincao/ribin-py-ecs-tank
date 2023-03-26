9727287ff020fc3083b0a81326270bdc54981fc1
ribincao <njucaolibin@163.com>
2023-03-26 20:44:41 +0800

    feat:scene关卡map

--- interface/view.py
+++ interface/view.py
@@ -10,8 +10,8 @@ class IView(object):
     def __init__(self, context: IContext):
         self.behaviors: Dict[int, IBehavior] = {}
         self.context: IContext = context
-        self.scenes: Dict[str, object] = {}
-        self.scene_id: str = ""  # 场景id, 不同关卡
+        self.scenes: Dict[int, object] = {}
+        self.scene_id: int = 0  # 场景id, 不同关卡
         self.back_ground = (0, 0, 0)
 
     @abstractmethod
