4e2f26eb6e6a95ccbe686d1b8f603379f8d0942f
bud-caolibin <caolibin@budcreate.io>
2023-04-07 18:21:50 +0800

    fix:碰撞检测系统pythonic

--- logic/system/collider_system.py
+++ logic/system/collider_system.py
@@ -12,60 +12,60 @@ class ColliderSystem(System):
 
     async def update(self):
         entities = self.context.get_entities()
-        for entityA in entities:
-            if not entityA.box_collider:
-                continue
-            # 先做个简单的,只判断玩家和子弹,后面抽象优化
-            if not entityA.model or entityA.model.model_name not in ["player1", "player2", "bullet"]:
+        for i in range(len(entities)):
+            entity_a = entities[i]
+            if not entity_a.box_collider or not entity_a.move:
                 continue
 
-            for entityB in entities:
-                if not entityB.box_collider:
+            for j in range(i + 1, len(entities)):
+                entity_b = entities[j]
+                if not entity_b.box_collider:
                     continue
-                if entityA.box_collider.layer != entityB.box_collider.layer:
-                    continue
-                if entityA.uid == entityB.uid:
+                if entity_a.box_collider.layer != entity_b.box_collider.layer:
                     continue
 
-                positionA = entityA.transform.position
-                heightA = entityA.box_collider.height
-                widthA = entityA.box_collider.width
+                position_a = entity_a.transform.position
+                heigh_a = entity_a.box_collider.height
+                width_a = entity_a.box_collider.width
 
-                positionB = entityB.transform.position
-                heightB = entityB.box_collider.height
-                widthB = entityB.box_collider.width
+                position_b = entity_b.transform.position
+                height_b = entity_b.box_collider.height
+                width_b = entity_b.box_collider.width
 
                 collider_direction, collider = 0, False
-                if positionA[0] >= positionB[0] and positionA[1] >= positionB[1]:
-                    distanceX = positionA[0] - positionB[0]
-                    distanceY = positionA[1] - positionB[1]
-                    if distanceX < widthB and distanceY < heightA:
-                        collider = True
-                        entityA.box_collider.collider_direction = (-1, -1)
-                        collider_direction = 1
-                elif positionA[0] >= positionB[0] and positionA[1] < positionB[1]:
-                    distanceX = positionA[0] - positionB[0]
-                    distanceY = positionB[1] - positionA[1]
-                    if distanceX < widthA and distanceY < heightA:
-                        collider = True
-                        entityA.box_collider.collider_direction = (1, -1)
-                        collider_direction = 2
-                        logger.debug(f"2. box collider detect {entityA.uid} -> {entityB.uid}")
-                elif positionA[0] < positionB[0] and positionA[1] < positionB[1]:
-                    distanceX = positionB[0] - positionA[0]
-                    distanceY = positionB[1] - positionA[1]
-                    if distanceX < widthA and distanceY < heightB:
-                        collider = True
-                        entityA.box_collider.collider_direction = (1, 1)
-                        collider_direction = 3
-                        logger.debug(f"3. box collider detect {entityA.uid} -> {entityB.uid}")
-                elif positionA[0] < positionB[0] and positionA[1] >= positionB[1]:
-                    distanceX = positionB[0] - positionA[0]
-                    distanceY = positionA[1] - positionB[1]
-                    if distanceX < widthB and distanceY < heightB:
-                        collider = True
-                        entityA.box_collider.collider_direction = (-1, 1)
-                        collider_direction = 4
+                if position_a[0] >= position_b[0] and position_a[1] >= position_b[1]:
+                    distance_x = position_a[0] - position_b[0]
+                    distance_y = position_a[1] - position_b[1]
+                    if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
+                        continue
+                    collider = True
+                    entity_a.box_collider.collider_direction = (-1, -1)
+                    collider_direction = 1
+                elif position_a[0] >= position_b[0] and position_a[1] < position_b[1]:
+                    distance_x = position_a[0] - position_b[0]
+                    distance_y = position_b[1] - position_a[1]
+                    if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
+                        continue
+                    collider = True
+                    entity_a.box_collider.collider_direction = (1, -1)
+                    collider_direction = 2
+                elif position_a[0] < position_b[0] and position_a[1] < position_b[1]:
+                    distance_x = position_b[0] - position_a[0]
+                    distance_y = position_b[1] - position_a[1]
+                    if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
+                        continue
+                    collider = True
+                    entity_a.box_collider.collider_direction = (1, 1)
+                    collider_direction = 3
+                elif position_a[0] < position_b[0] and position_a[1] >= position_b[1]:
+                    distance_x = position_b[0] - position_a[0]
+                    distance_y = position_a[1] - position_b[1]
+                    if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
+                        continue
+                    collider = True
+                    entity_a.box_collider.collider_direction = (-1, 1)
+                    collider_direction = 4
 
                 if collider:
-                    logger.debug(f"box collider detect {collider_direction}: {entityA.uid} -> {entityB.uid}")
+                    logger.info(f"box collider detect {collider_direction}: {entity_a.uid} -> {entity_b.uid}")
+
