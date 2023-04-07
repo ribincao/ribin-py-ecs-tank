f244335316c7f73e88d49a9008d19ade614310f1
bud-caolibin <caolibin@budcreate.io>
2023-04-07 18:29:56 +0800

    fix:完善碰撞检测系统

--- logic/system/collider_system.py
+++ logic/system/collider_system.py
@@ -1,8 +1,6 @@
 from logic.interface.system import System
 from logic.context import Context
 from common.logger import logger
-from logic.entity.entity import GameLogicEntity
-from logic.component.state_component import State
 
 
 class ColliderSystem(System):
@@ -32,40 +30,36 @@ class ColliderSystem(System):
                 height_b = entity_b.box_collider.height
                 width_b = entity_b.box_collider.width
 
-                collider_direction, collider = 0, False
+                collider_direction = [0, 0]
                 if position_a[0] >= position_b[0] and position_a[1] >= position_b[1]:
                     distance_x = position_a[0] - position_b[0]
                     distance_y = position_a[1] - position_b[1]
                     if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
                         continue
-                    collider = True
-                    entity_a.box_collider.collider_direction = (-1, -1)
-                    collider_direction = 1
+                    collider_direction = [-1, -1]
                 elif position_a[0] >= position_b[0] and position_a[1] < position_b[1]:
                     distance_x = position_a[0] - position_b[0]
                     distance_y = position_b[1] - position_a[1]
                     if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
                         continue
-                    collider = True
-                    entity_a.box_collider.collider_direction = (1, -1)
-                    collider_direction = 2
+                    collider_direction = [-1, 1]
                 elif position_a[0] < position_b[0] and position_a[1] < position_b[1]:
                     distance_x = position_b[0] - position_a[0]
                     distance_y = position_b[1] - position_a[1]
                     if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
                         continue
-                    collider = True
-                    entity_a.box_collider.collider_direction = (1, 1)
-                    collider_direction = 3
+                    collider_direction = [1, 1]
                 elif position_a[0] < position_b[0] and position_a[1] >= position_b[1]:
                     distance_x = position_b[0] - position_a[0]
                     distance_y = position_a[1] - position_b[1]
                     if not (distance_x < (width_a + width_b) / 2 and distance_y < (heigh_a + height_b) / 2):
                         continue
-                    collider = True
-                    entity_a.box_collider.collider_direction = (-1, 1)
-                    collider_direction = 4
+                    collider_direction = [1, -1]
 
-                if collider:
-                    logger.info(f"box collider detect {collider_direction}: {entity_a.uid} -> {entity_b.uid}")
+                if position_a[0] == position_b[0]:
+                    collider_direction[0] = 0
+                if position_a[1] == position_b[1]:
+                    collider_direction[1] = 0
+
+                logger.info(f"box collider detect {collider_direction}: {entity_a.uid} -> {entity_b.uid}")
 
