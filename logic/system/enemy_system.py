be59ddce98b58dd8d24af4a66311e14563c57d93
ribincao <njucaolibin@163.com>
2023-04-08 00:24:35 +0800

    fix:让机器人稍微随机一点

--- logic/system/enemy_system.py
+++ logic/system/enemy_system.py
@@ -2,6 +2,7 @@ from logic.interface.system import System
 from logic.context import Context
 from logic.entity.entity import GameLogicEntity
 from typing import List, Optional, Tuple
+import random
 
 
 class EnemySystem(System):
@@ -12,6 +13,9 @@ class EnemySystem(System):
         self.slot: List[Optional[GameLogicEntity]] = [None for _ in range(self.MAX_CNT)]
         self.positions: List[Tuple[float, float]] = [(0, 0), (360, 0), (720, 0)]
 
+    def get_random_rotate(self):
+        return random.choice([0, 90, 180, 270])
+
     async def update(self):
         entities = self.context.get_entities()
         for entity in entities:
@@ -21,6 +25,5 @@ class EnemySystem(System):
                 continue
             if entity.transform.last_position != entity.transform.position:
                 continue
-            entity.transform.rotation = (entity.transform.rotation + 90) % 360
-        pass
+            entity.transform.rotation = self.get_random_rotate()
 
