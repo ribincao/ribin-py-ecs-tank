d247685c24cf2bd83886586ab7132020ba1a1af0
bud-caolibin <caolibin@budcreate.io>
2023-04-11 11:13:07 +0800

    feat:参考C#增加一种新的事件实现

--- logic/event/event.py
+++ logic/event/event.py
@@ -1,6 +1,27 @@
 from typing import Callable, Dict, List
 
 
+class Event(object):
+
+    def __init__(self):
+        self._handlers: List[Callable] = []
+
+    def __call__(self, *args, **kwargs):
+        for handler in self._handlers:
+            handler(*args, **kwargs)
+
+    def __add__(self, handler: Callable):
+        if handler not in self._handlers:
+            self._handlers.append(handler)
+        return self
+
+    def __sub__(self, handler: Callable):
+        if handler in self._handlers:
+            self._handlers.remove(handler)
+        return self
+
+
+
 class IEvent(object):
 
     def __init__(self):
