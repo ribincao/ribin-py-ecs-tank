f296515574b89e620cbb4b044fa72491956a2e32
bud-caolibin <caolibin@budcreate.io>
2023-04-11 10:58:44 +0800

    feat:增加match

---
+++ logic/match/matcher.py
@@ -0,0 +1,19 @@
+from typing import Optional, List
+from logic.entity.entity import GameLogicEntity
+
+
+class Matcher(object):
+
+    def __init__(self, *t_comps, **kwargs):
+        self._all: Optional[List[str]] = list(t_comps) if t_comps else kwargs.get("all_of", None)
+        self._any: Optional[List[str]] = kwargs.get("any_of", None)
+        self._none: Optional[List[str]] = kwargs.get("none_of", None)
+
+    def matches(self, entity: GameLogicEntity):
+        all_condition = self._all is None or entity.has(*self._all)
+        any_condition = self._any is None or entity.has_any(*self._any)
+        none_condition = self._none is None or not entity.has_any(*self._none)
+        return all_condition and any_condition and none_condition
+
+
+
